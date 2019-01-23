2c3fbd9a7fa9b55f2c3c7c61ea0de15dc8c664f8
roydejager <roydejager@live.nl>
2019-01-23 13:15:14 +0100

    add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,28 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.min.js'
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
       }
     ]
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+    new HtmlWebpackPlugin({
+      template: './index.html'
     })
   ]
-};
+}
